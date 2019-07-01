ed28e268605ff3e2cf5baa72c5accd7102c09a2c
jjeda <jjeda@naver.com>
2019-07-01 22:16:25 +0900

    step1 코드

---
+++ src/main/java/me/jjeda/accountexample/domain/Account.java
@@ -0,0 +1,65 @@
+package me.jjeda.accountexample.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.jjeda.accountexample.dto.AccountDto;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "account")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name ="email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name ="first_name", nullable = false)
+    private String firstName;
+
+    @Column(name ="last_name", nullable = false)
+    private String lastName;
+
+    @Column(name ="password", nullable = false)
+    private String password;
+
+    @Column(name ="address1")
+    private String address1;
+
+    @Column(name ="address2")
+    private String address2;
+
+    @Column(name ="zip",nullable = false)
+    private String zip;
+
+    @Column(name ="created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name ="updated_at")
+    private LocalDateTime upadtedAt;
+
+    @Builder
+    public Account(String email, String firstName, String lastName, String password, String address1, String address2, String zip) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.address1 = address1;
+        this.address2 = address2;
+        this.zip = zip;
+    }
+
+    public void updateMyAccount(AccountDto.MyAccountReq dto) {
+        this.address1 = dto.getAddress1();
+        this.address2 = dto.getAddress2();
+        this.zip = dto.getZip();
+    }
+}
---
+++ src/main/java/me/jjeda/accountexample/dto/AccountDto.java
@@ -0,0 +1,83 @@
+package me.jjeda.accountexample.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import me.jjeda.accountexample.domain.Account;
+
+public class AccountDto {
+
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class SignUpReq {
+        private String email;
+        private String firstName;
+        private String lastName;
+        private String password;
+        private String address1;
+        private String address2;
+        private String zip;
+
+        @Builder
+        public SignUpReq(String email, String firstName, String lastName, String password, String address1, String address2, String zip) {
+            this.email = email;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.password = password;
+            this.address1 = address1;
+            this.address2 = address2;
+            this.zip = zip;
+        }
+
+        public Account toEntity() {
+            return Account.builder()
+                    .email(this.email)
+                    .firstName(this.firstName)
+                    .lastName(this.lastName)
+                    .password(this.password)
+                    .address1(this.address1)
+                    .address2(this.address2)
+                    .zip(this.zip)
+                    .build();
+        }
+
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class MyAccountReq {
+        private String address1;
+        private String address2;
+        private String zip;
+
+        @Builder
+        public MyAccountReq(String address1, String address2, String zip) {
+            this.address1 = address1;
+            this.address2 = address2;
+            this.zip = zip;
+        }
+    }
+
+    @Getter
+    public static class Res {
+        private String email;
+        private String firstName;
+        private String lastName;
+        private String password;
+        private String address1;
+        private String address2;
+        private String zip;
+
+        public Res(Account account) {
+            this.email = account.getEmail();
+            this.firstName = account.getFirstName();
+            this.lastName = account.getLastName();
+            this.password = account.getPassword();
+            this.address1 = account.getAddress1();
+            this.address2 = account.getAddress2();
+            this.zip = account.getZip();
+        }
+    }
+}
